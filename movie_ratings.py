f0e39baf9d2d2604aae063eaf3b45a5a476c62d6
IAB24 <ilyasabshir20@gmail.com>
2023-01-31 15:31:45 +0000

    add text

--- movie_ratings.py
+++ movie_ratings.py
@@ -2,12 +2,12 @@ age = input("What is your age? ")
 
 if age.isdigit() and int(age) > 0 and int(age) < 117:
     if int(age) >= 18:
-        print("You are old enough to buy tickets for any movie")
+        print("You are old enough to buy a ticket for any movie ")
     elif int(age) < 18 and int(age) >= 15:
-        print("You can buy tickets for films rated U, PG, 12 and 15")
+        print("You can buy a ticket for films rated U, PG, 12 and 15")
     elif int(age) < 15 and int(age) >=12:
         print("You can buy tickets for films rated U, PG, and 12")
     else:
-        print("You can buy tickets for any U rated films but adult supervision for PG rated films")
+        print("You can buy tickets for any U rated films but adult is supervision still advised ")
 else:
     print("The value you've entered is invalid, please enter your age in digits")
